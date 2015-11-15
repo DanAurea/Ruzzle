b46c24d55516422f0b41fdc3089c1169e64101ea
Brandon/DanAurea <laochi.bc@gmail.com>
2015-11-15 02:04:26 +0100

    Ajout d'un fichier txt avec le résultat du solver.

--- src/ruzzle.c
+++ src/ruzzle.c
@@ -18,8 +18,14 @@ int main(int argc, char * argv[]){
 	
 	Grille(grid, argc, argv); // Affiche la grille
 	Solver(grid); // Résoud la grille
+
+	fprintf(stdout,"La liste a été générée dans assets/listWords.txt\n");
+	freopen("assets/listWords.txt", "w", stdout); // Redirige la sortie standard
+
+	puts("La liste des mots est: \n");
 	printList(); // Affiche les mots trouvés du plus grand au plus petit
 	clearList(); // Libère la liste de la mémoire
+	fclose(stdout);
 	
 	return 0;
 }
